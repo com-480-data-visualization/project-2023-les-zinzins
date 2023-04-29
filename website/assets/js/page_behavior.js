a18b840adc70236f61a634df92a50ba7cf23b4ef
Gaston Wolfart <gastonwolfart@gmail.com>
2023-04-29 15:16:54 +0200

    added active section tracking

---
+++ website/assets/js/page_behavior.js
@@ -0,0 +1,47 @@
+// Script to enable smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+// Script to have the navbar follow the active section
+const sections = document.querySelectorAll("section[id]");
+const rooms = document.querySelectorAll(".room");
+
+const mainContainer = document.querySelector('main');
+
+mainContainer.onscroll = function () { scrollHandler() };
+
+function scrollHandler() {
+    let scrollY = mainContainer.scrollTop;
+
+    // console.log(scrollY);
+
+    // Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 100;
+        sectionId = current.getAttribute("id");
+
+
+        /*
+        - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+        - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+        */
+        if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + sectionHeight
+        ) {
+            console.log(sectionId);
+            document.querySelector("#" + sectionId + "_room").classList.add("active");
+        } else {
+            document.querySelector("#" + sectionId + "_room").classList.remove("active");
+        }
+    });
+}
