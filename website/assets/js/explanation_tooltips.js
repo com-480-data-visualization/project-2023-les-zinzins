9766e6ed7d75676c7e78a0bffbd1c84a83e896a5
Gaston Wolfart <gastonwolfart@gmail.com>
2023-05-28 20:13:04 +0200

    added explanation buttons

---
+++ website/assets/js/explanation_tooltips.js
@@ -0,0 +1,31 @@
+tippy('#section_1>h1>svg', {
+    placement: 'bottom',
+    interactive: true,
+    delay: [0, 500],
+    content: 'Add explanation here.',
+    theme: 'light-border',
+});
+
+tippy('#section_2>h1>svg', {
+    placement: 'bottom',
+    interactive: true,
+    delay: [0, 500],
+    content: 'Add explanation here.',
+    theme: 'light-border',
+});
+
+tippy('#section_3>h1>svg', {
+    placement: 'bottom',
+    interactive: true,
+    delay: [0, 500],
+    content: 'Add explanation here.',
+    theme: 'light-border',
+});
+
+tippy('#section_4>h1>svg', {
+    placement: 'bottom',
+    interactive: true,
+    delay: [0, 500],
+    content: 'Hover over the city circles to show the average daily temperature over the year of 2022 for that city.',
+    theme: 'light-border',
+});
